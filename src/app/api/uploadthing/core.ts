3a9e62d928e3a19359af97b075df0e3cf13566cf
Ronin-Chan <yuhaochen37@outlook.com>
2024-01-05 16:26:19 +1100

    feat: finish chat feature

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,10 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { pinecone } from "@/lib/pinecone";
  
 const f = createUploadthing();
  
@@ -36,6 +40,47 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING'
         }
       })
+
+      try {
+        const response = await fetch(file.url);
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        const pineconeIndex = pinecone.Index("quill")
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY!,
+        })
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        })
+
+        await db.file.update({
+          where: {
+            id: createdFile.id
+          },
+          data: {
+            uploadStatus: 'SUCCESS',
+          }
+        })
+      } catch (error) {
+        console.error(error)
+        await db.file.update({
+          where: {
+            id: createdFile.id
+          },
+          data: {
+            uploadStatus: 'FAILED',
+          }
+        })
+      }
     }),
 } satisfies FileRouter;
  
--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -6,6 +6,8 @@ import Message from "./Message"
 import { ChevronLeft, Frown, Loader2 } from "lucide-react"
 import Link from "next/link"
 import { buttonVariants } from "../ui/button"
+import { ChatContextProvider } from "./ChatContext"
+import Messages from "./Messages"
 
 const ChatWrapper = ({ fileId }: { fileId: string }) => {
   const { data, isLoading } = trpc.getFileUploadStatus.useQuery({
@@ -80,12 +82,15 @@ const ChatWrapper = ({ fileId }: { fileId: string }) => {
     )
 
   return (
-    <div>
-      <div>
-        <Message />
+    <ChatContextProvider fileId={fileId}>
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 justify-between flex flex-col mb-28'>
+          <Messages fileId={fileId} />
+        </div>
+
+        <ChatInput />
       </div>
-      <ChatInput isDisabled />
-    </div>
+    </ChatContextProvider>
   )
 }
 
